7262245015be539ec20a806e5972a288a08c6756
Afnan Enayet <afnanenayet@users.noreply.github.com>
2014-10-25 13:47:42 -0400

    completed assignment
    added to stub file

--- cachematrix.R
+++ cachematrix.R
@@ -12,4 +12,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse_R <- x$getinverse()
+        if(!is.null(inverse_R)) {
+           return inverse_R
+        }
+        
+        data <- x$get()
+        inverse_R <- solve(data, ...)
+        x$setinverse(inverse_R)
+        inverse_R
+        
 }
